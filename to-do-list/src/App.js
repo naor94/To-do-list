eea3f8849af6d571a7b580b3d5e88cf187781327
naor94 <44939883+naor94@users.noreply.github.com>
2022-01-10 09:43:56 +0200

    j

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import CreateNote from './components/CreateNote';
 import Header from './components/Header';
+import EditableTable from './components/Table';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <CreateNote/>
+      <EditableTable/>
     </div>
   );
 }
